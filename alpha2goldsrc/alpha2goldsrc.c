aec0d537bd19ef32e710fd947514c2144d462f7a
shinola <eoscini@icloud.com>
2020-05-07 19:29:25 +1000

    finished alpha2goldsrc!!

--- alpha2goldsrc/alpha2goldsrc.c
+++ alpha2goldsrc/alpha2goldsrc.c
@@ -3,15 +3,15 @@
  * converts 0.52 maps to goldsrc-compatible maps
  */
 #include <stdio.h>
-
-#define BSP_ALPHA	0x1D
-#define BSP_RETAIL	0x1E
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-	unsigned char bspheader[0];
-	const char *bspname;
-	FILE *bspfile;
+	unsigned char alphaheader;
+	unsigned char retailheader;
+	
+	char *filename;
+	FILE *filestream;
 	
 	printf("shinola's modded alpha tools\n");
 	printf("---- alpha2goldsrc ----\n");
@@ -22,44 +22,54 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	
-	/* open the bspfile and the file stream */
-	bspname = argv[1];
-	printf("opening %s...\n", bspname);
-	bspfile = fopen(bspname, "r+w");
+	filename = argv[1];
 	
-	if (!bspfile)
+	/* check if there isn't a .bsp extension so it can be appended */
+	if ((strrchr(filename, '.')) == NULL)
 	{
-		printf("ERROR: unable to open %s!\n", bspname);
+		/* if it doesn't end in '.bsp' */
+		if(strcmp(filename, ".bsp") == 1)
+		{
+			strcat(filename, ".bsp");
+		}
+	}
+	
+	/* open the bsp file and the file stream */
+	printf("opening %s...\n", filename);
+	filestream = fopen(filename, "r+");
+	
+	if (!filestream)
+	{
+		printf("ERROR: unable to open %s!\n", filename);
 		return 1;
 	}
 	
 	/* read the bsp header */
 	printf("reading header...\n");
 	
-	fseek(bspfile, 0, SEEK_SET);
-	fread(bspheader, sizeof(unsigned char), 1, bspfile);
+	fseek(filestream, 0, SEEK_SET);
+	fread(&alphaheader, sizeof(unsigned char), 1, filestream);
 	
 	/* display the header and double check if we've got an alpha bsp */
-	if (bspheader[0] = BSP_ALPHA)
+	if (alphaheader == 0x1D) /* 0x1D = alpha bsp */
 	{
-		printf("opened %s!\n\n", bspname);
+		printf("opened %s!\n\n", filename);
 	}
 	else
 	{
-		printf("ERROR: %s has an invalid bsp version!\n", bspname);
+		printf("ERROR: %s has an invalid bsp version!\n", filename);
 		return 1;
 	}
 	
 	/* write retail bsp header into the first offset */
-	printf("converting %s...\n", bspname);
+	printf("converting %s...\n", filename);
 	
-	/*
-	 * TODO: the conversion code itself is incomplete.
-	 * i need to find a way to properly convert it
-	 */
+	retailheader = 0x1E;
+	fseek(filestream, 0, SEEK_SET);
+	fwrite(&retailheader, sizeof(unsigned char), 1, filestream);
 	
 	/* we've finished everything up, now time to close the stream */
-	printf("%s has been converted!\n", bspname);
-	fclose(bspfile);
+	printf("%s has been converted!\n", filename);
+	fclose(filestream);
 	return 0;
 }
